6f6b664ae8f0626d0bbdb3747c2ed9d51a67196a
hosseinnoshadi2006-hue <hossein.noshadi.2006@gmail.com>
2026-09-11 02:45:54 +0330

    Create main.py

---
+++ main.py
@@ -0,0 +1,52 @@
+import os
+import requests
+from fastapi import FastAPI, Request
+
+app = FastAPI()
+
+TOKEN = os.environ["BOT_TOKEN"]
+TELEGRAM_API = f"https://api.telegram.org/bot{TOKEN}"
+
+REPLY = """سلام 👋
+پیام شما دریافت شد. در اولین فرصت پاسخ خواهم داد."""
+
+
+@app.get("/")
+def home():
+    return {"status": "running"}
+
+
+@app.post("/telegram")
+async def telegram_webhook(request: Request):
+
+    update = await request.json()
+
+    message = update.get("business_message")
+
+    if not message:
+        return {"ok": True}
+
+    business_connection_id = message.get(
+        "business_connection_id"
+    )
+
+    chat = message.get("chat")
+
+    if not business_connection_id or not chat:
+        return {"ok": True}
+
+    chat_id = chat["id"]
+
+    data = {
+        "business_connection_id": business_connection_id,
+        "chat_id": chat_id,
+        "text": REPLY
+    }
+
+    requests.post(
+        f"{TELEGRAM_API}/sendMessage",
+        json=data,
+        timeout=10
+    )
+
+    return {"ok": True}
