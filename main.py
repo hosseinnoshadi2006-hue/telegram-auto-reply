ec37a456ba20463c837d315cb25bf49cd02c4f8b
hosseinnoshadi2006-hue <hossein.noshadi.2006@gmail.com>
2026-09-11 03:01:24 +0330

    Update main.py

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ TELEGRAM_API = f"https://api.telegram.org/bot{TOKEN}"
 REPLY = """سلام 👋
 پیام شما دریافت شد. در اولین فرصت پاسخ خواهم داد."""
 
+replied_users = set()
+
 
 @app.get("/")
 def home():
@@ -26,16 +28,17 @@ async def telegram_webhook(request: Request):
     if not message:
         return {"ok": True}
 
-    business_connection_id = message.get(
-        "business_connection_id"
-    )
-
+    business_connection_id = message.get("business_connection_id")
     chat = message.get("chat")
 
     if not business_connection_id or not chat:
         return {"ok": True}
 
     chat_id = chat["id"]
+
+    # اگر قبلاً به این شخص جواب داده‌ایم، دوباره جواب نده
+    if chat_id in replied_users:
+        return {"ok": True}
 
     data = {
         "business_connection_id": business_connection_id,
@@ -48,5 +51,7 @@ async def telegram_webhook(request: Request):
         json=data,
         timeout=10
     )
+
+    replied_users.add(chat_id)
 
     return {"ok": True}
